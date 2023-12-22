2bc61323fb49e5bb9ac93ff98c5de8194732d5dc
MirnaMohamed <mirnaattallah00@gmail.com>
2023-12-23 01:18:28 +0200

    Autokey Decryption

--- SecurityProject/algorithms/Autokey_Cipher.cs
+++ SecurityProject/algorithms/Autokey_Cipher.cs
@@ -1,12 +1,4 @@
-﻿
-
-// A C# program to illustrate
-// Autokey Cipher Technique
-using SecurityProject.algorithms;
-using SecurityProject.algorithms.Interfaces;
-using SecurityProject.Exceptions;
-using System.Text;
-
+﻿using SecurityProject.algorithms.Interfaces;
 
 
  namespace SecurityProject.algorithms
@@ -20,63 +12,49 @@ public class AutoKey:ICipher
     {
         
          this.key = Program.MessageToCode(key);
-
-        // This if statement is all about C# regular
-        // expression
-        // [] for range
-        // // Extra \ is used to escape one \
-        // \\d acts as delimiter
-        // ? once or not at all
-        // . Any character (may or may not match line
-        // terminators)
     
     }
-
-        public string Decrypt(int[] textCode)
+        public string Encrypt(int[] message)
         {
-            throw new NotImplementedException();
+            int len = message.Length;
+
+            // generating the keystream
+            int[] newKey = new int[message.Length];
+            Array.Copy(key, key.GetLowerBound(0), newKey, newKey.GetLowerBound(0), key.Length);
+            Array.Copy(message, message.GetLowerBound(0), newKey, key.Length, message.Length - key.Length);
+            int[] encryptMsg = new int[message.Length];
+
+            // applying encryption algorithm
+            for (int x = 0; x < len; x++)
+            {
+                int first = message[x];
+                int second = newKey[x];
+                int total = (first + second) % 95;
+                encryptMsg[x] = total;
+            }
+            return Program.CodeToMessage(encryptMsg);
         }
-
-        public  string Encrypt(int[] message)
-    {
-        int len = message.Length;
-        
-        // generating the keystream
-        int[] newKey =new int[message.Length];
-        Array.Copy(key, key.GetLowerBound(0), newKey , newKey .GetLowerBound(0), key.Length);
-        Array.Copy(message, message.GetLowerBound(0), newKey , newKey .GetLowerBound(key.Length), message.Length-key.Length);
-       // newKey = newKey.Substring(0, newKey.Length  - key.Length);
-        int[] encryptMsg = new int[message.Length];
-
-        // applying encryption algorithm
-        for (int x = 0; x < len; x++)
+        public string Decrypt(int[] encryptedMsg)
         {
-            int first = message[x];
-            int second = newKey[x];
-            int total = (first + second) % 94;
-            encryptMsg [x]= total;
+            int[] currentKey = new int[encryptedMsg.Length];
+            int[] decryptMsg = new int[encryptedMsg.Length];
+            Array.Copy(key, key.GetLowerBound(0), currentKey, currentKey.GetLowerBound(0), key.Length);
+            //applying decryption algorithm
+            for (int x = 0; x < encryptedMsg.Length; x++)
+            {
+                int encryptedChar = encryptedMsg[x];
+                int keyChar;
+                if (x == 0)
+                    keyChar = currentKey[x];
+                else
+                    keyChar = currentKey[x - 1];
+                int total = (encryptedChar - keyChar) % 95;
+                total = (total < 0) ? total + 95 : total;
+                decryptMsg[x] = total;
+                currentKey[x] = total;
+            }
+            return Program.CodeToMessage(decryptMsg);
         }
-        return  Program.CodeToMessage(encryptMsg);
     }
-
-    // public static string Decrypt(string message,
-    //                                     string key)
-    // {
-    //     string currentKey = key;
-    //     string decryptMsg = "";
-
-        // applying decryption algorithm
-    //     for (int x = 0; x < message.Length; x++)
-    //     {
-    //         int get1 = codeList.IndexOf(message[x]);
-    //         int get2 = codeList.IndexOf(currentKey[x]);
-    //         int total = (get1 - get2) % 94;
-    //         total = (total < 0) ? total + 94 : total;
-    //         decryptMsg += codeList[total];
-    //         currentKey += codeList[total];
-    //     }
-    //     return decryptMsg;
-    // }
-}
 
 }
